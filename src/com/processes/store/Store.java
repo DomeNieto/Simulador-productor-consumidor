63d88338e827a7cc8bed66b7babe3f72577c063a
ori05paz <159430429+ori05paz@users.noreply.github.com>
2024-11-22 20:40:09 +0000

    fix(Store): Changed collect method for consumed and added logic

--- src/com/processes/store/Store.java
+++ src/com/processes/store/Store.java
@@ -1,28 +1,38 @@
 package com.processes.store;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Store {
     private int limit;
+    private final List<String> storage;
 
-    public Store(){
+    public Store() {
         this.limit = 20;
+        this.storage = new ArrayList<>();
     }
 
-    public int getLimit(){
+    public int getLimit() {
         return limit;
     }
 
-    public synchronized void deposit(){
-        this.limit++;
+    public synchronized void deposit(String vegetable, String nameFarmer) throws InterruptedException {
+        while (this.storage.size() >= limit) {
+            wait();
+        }
+        storage.add(vegetable);
+        notifyAll();
     }
 
-    public synchronized void collect(){
-       while (this.limit <= 0) {
-        try {
+    public synchronized void consumes(String client) throws InterruptedException {
+
+        while (storage.isEmpty()) {
+            System.out.println(client + " has to wait, there are no vegetables available in stock");
             wait();
-        } catch (InterruptedException e) {
-            System.err.println("Hilo interrumpido durante la espera.");
         }
-       }
-       this.limit--;
+
+        String vegetable = storage.remove(0);
+        System.out.println(client + " consumed: " + vegetable);
+        notifyAll();
     }
 }

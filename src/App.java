21c177904e713ed46c0e2416dafd49820ed6e2f8
ori05paz <159430429+ori05paz@users.noreply.github.com>
2024-11-22 22:52:56 +0000

    feat(App): Created main logic.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,38 @@
+import com.processes.store.Store;
+import com.processes.thread.Client;
+import com.processes.thread.Farmer;
+import com.processes.util.Colors;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Store store = new Store();
+
+        Farmer paco = new Farmer("Paco", store);
+        Farmer ramon = new Farmer("Ramon", store);
+
+        Client faustino = new Client(store, "Faustino", 5);
+        Client gentleman = new Client(store, "Mr.Gentleman", 10);
+        Client loquendo = new Client(store, "Loquendo", 5);
+
+        paco.start();
+        ramon.start();
+
+        faustino.start();
+        gentleman.start();
+        loquendo.start();
+
+        try {
+            paco.join();
+            ramon.join();
+            faustino.join();
+            gentleman.join();
+            loquendo.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(Colors.ANSI_YELLOW + "\n" + "________________________________________________________________________________" +  Colors.ANSI_RESET);
+        System.out.println(Colors.ANSI_YELLOW + "________________________________________________________________________________" +  Colors.ANSI_RESET);
+        System.out.println(Colors.ANSI_YELLOW + "\t\t\t\t" + "Store Closed" + Colors.ANSI_RESET);
     }
 }

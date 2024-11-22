6a9f4ee854f02b5489a409037ef542ffe76b650b
DomenicaNieto <usr3857@salesianos-lacuesta.net>
2024-11-22 19:59:41 +0000

    fix(Store): Added storage list attribute and modified deposit element to be able to be used properly in Farmer class.

--- src/com/processes/store/Store.java
+++ src/com/processes/store/Store.java
@@ -1,28 +1,37 @@
 package com.processes.store;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Store {
     private int limit;
+    private final List<String> storage;
 
-    public Store(){
+    public Store() {
         this.limit = 20;
+        this.storage = new ArrayList<>();
     }
 
-    public int getLimit(){
+    public int getLimit() {
         return limit;
     }
 
-    public synchronized void deposit(){
-        this.limit++;
+    public synchronized void deposit(String vegetable, String nameFarmer) throws InterruptedException {
+        while (this.storage.size() >= limit) {
+            wait();
+        }
+        storage.add(vegetable);
+        notifyAll();
     }
 
-    public synchronized void collect(){
-       while (this.limit <= 0) {
-        try {
-            wait();
-        } catch (InterruptedException e) {
-            System.err.println("Hilo interrumpido durante la espera.");
+    public synchronized void collect() {
+        while (this.limit <= 0) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                System.err.println("Hilo interrumpido durante la espera.");
+            }
         }
-       }
-       this.limit--;
+        this.limit--;
     }
 }
